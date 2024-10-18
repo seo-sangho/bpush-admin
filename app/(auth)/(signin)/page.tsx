3182b30626eb1ca6f57c899b023ac1bb06260405
seo-sangho <ssh0702@gmail.com>
2024-10-18 11:06:29 +0900

    TEST: Vercel 반영 확인

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -40,12 +40,8 @@ export default function AuthenticationPage() {
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <p className="text-lg">&ldquo;Just Do It.&rdquo;</p>
+            <footer className="text-sm"></footer>
           </blockquote>
         </div>
       </div>
